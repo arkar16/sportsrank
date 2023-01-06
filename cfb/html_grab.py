268da2fc23d0ffb042fae22754c9430eb92d7ff2
Arya Karnik <abkarnik@crimson.ua.edu>
2023-01-05 20:07:06 -0700

    index.html fixes

--- cfb/html_grab.py
+++ cfb/html_grab.py
@@ -14,14 +14,14 @@ def html_grab(start_year, end_year, division):
     title_html += f"<h1>CORS {config.cors_version} CFB Results</h1>\n" # TODO change CFB to SPORT variable
     title_html += "</body>\n"
     title_html += "</html>\n"
-    html_nc = f'<a href="sportsrank/cfb/years/history/CORS_NC_{division}_CFB">Link to all {division} CFB National Champions</a><br>\n'
-    html_wt = f'<a href="sportsrank/cfb/years/history/CORS_WT_{division}_CFB">Link to all {division} CFB Worst Teams</a><br>\n'
+    html_nc = f'<a href="/cfb/years/history/CORS_NC_{division}_CFB">Link to all {division} CFB National Champions</a><br>\n'
+    html_wt = f'<a href="/cfb/years/history/CORS_WT_{division}_CFB">Link to all {division} CFB Worst Teams</a><br>\n'
     with open("index.html", "a") as f:
             f.write(title_html)
             f.write(html_nc)
             f.write(html_wt)
     for year in range(end_year, start_year, -1):
-        html_template = f'<a href="sportsrank/cfb/years/{year}/rankings/{year}_FINAL_{division}_cors.html">Link to {year} {division} CFB final rankings</a><br>\n'
+        html_template = f'<a href="/cfb/years/{year}/rankings/{year}_FINAL_{division}_cors.html">Link to {year} {division} CFB final rankings</a><br>\n'
         with open("index.html", "a") as f:
             f.write(html_template)
 
