9142ff604938b00154217c500a33d090d3506ea5
Arya Karnik <abkarnik@crimson.ua.edu>
2023-01-12 19:23:06 -0600

    working website

--- cfb/html_grab.py
+++ cfb/html_grab.py
@@ -26,8 +26,8 @@ def html_grab(start_year, end_year, division):
     title_html += f"<h1>CORS {config.cors_version} CFB Results</h1>\n" # TODO change CFB to SPORT variable
     title_html += "</body>\n"
     title_html += "</html>\n"
-    html_nc = f'<a href="cfb/years/history/nc_{division}_CFB_output.html"><h1>Link to all-time {division} CFB National Champions</h1></a><br>\n' # TODO CFB to {SPORT}
-    html_wt = f'<a href="cfb/years/history/wt_{division}_CFB_output.html"><h1>Link to all-time {division} CFB Worst Teams</h1></a><br>\n' # TODO CFB to {SPORT}
+    html_nc = f'<a href="../../../cfb/years/history/nc_{division}_CFB_output.html"><h1>Link to all-time {division} CFB National Champions</h1></a>\n' # TODO CFB to {SPORT}
+    html_wt = f'<a href="../../../cfb/years/history/wt_{division}_CFB_output.html"><h1>Link to all-time {division} CFB Worst Teams</h1></a>\n' # TODO CFB to {SPORT}
     html_years = f"<h1>Yearly CFB Results:</h1>\n" # TODO change CFB to SPORT variable
     with open("website/cfb/cfb.html", "w") as f: # TODO change CFB to {SPORT}
             f.write(title_html)
@@ -35,7 +35,7 @@ def html_grab(start_year, end_year, division):
             f.write(html_wt)
             f.write(html_years)
     for year in range(end_year, start_year - 1, -1): # FIXME links are broken from here down
-        html_template = f'<a href="sportsrank/cfb/years/{year}/rankings/{year}_FINAL_{division}_cors.html">Link to {year} {division} CFB final rankings</a><br>\n'
+        html_template = f'<a href="../../../cfb/years/{year}/rankings/{year}_FINAL_{division}_cors.html">Link to {year} {division} CFB final rankings</a><br>\n'
         year_template = f'<a href="years/{year}_CFB.html">Link to {year} {division} CFB</a><br>\n'
         with open(f"website/cfb/years/{year}_CFB.html", "w") as f: # TODO change CFB to {SPORT}
             f.write(html_template)
