28144db5b79dec7c4f15c85b9b045d2f8690ccdf
Arya Karnik <abkarnik@crimson.ua.edu>
2023-11-18 17:10:05 -0600

    removed SIMS

--- webconfig.py
+++ webconfig.py
@@ -1,7 +1,5 @@
 # config for website
 
-sports = "cfb, simcbb, simcfb" # current sports
+sports = "cfb" # current sports
 cfb_divisions = "FBS" # current CFB divisions
-simcbb_divisions = "D1" # current simCBB divisions
-simcfb_divisions = "D1" # current simCBB divisions
 cors_version = "v0.2.0" # current version of CORS
